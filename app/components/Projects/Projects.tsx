4f87024b13de1db89e35c0ccf34e9bc822c581fc
Yushw <86692796+DeletedUser9@users.noreply.github.com>
2026-03-09 12:09:52 +1300

    created projects section

---
+++ app/components/Projects/Projects.tsx
@@ -0,0 +1,9 @@
+import SectionHeader from "../GeneralComponents/SectionHeader"
+
+export default function About() {
+  return (
+    <div className='px-6 py-12 text-white'>
+        <SectionHeader title="Projects"/> 
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Hero />
-      <div id = "about">
-        <About />
+      <div id = "projects">
+        <Projects />
       </div>
-       
     </main>
   );
 }
