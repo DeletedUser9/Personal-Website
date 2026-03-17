2bac4f3d8d57fbbefee6ff09649d70d072e10a31
deletedUser <86692796+DeletedUser9@users.noreply.github.com>
2026-03-17 23:43:47 +1300

    Added CV link
    can now download cv

--- app/components/GeneralComponents/Hover.tsx
+++ app/components/GeneralComponents/Hover.tsx
@@ -5,17 +5,18 @@ type HoverProps = {
     text: string;
     isbutton: boolean;
     className?: string;
+    download?: string
     
     
 }
 
-export default function Hover( {href, text, isbutton, className}: HoverProps) {
+export default function Hover( {href, text, isbutton, className, download}: HoverProps) {
 
     const isbuttonclass = isbutton ? className  : "hover:text-green-300";
     
   return (
     <div>
-      <Link className={isbuttonclass} href={href}>{text}</Link>
+      <Link className={isbuttonclass} download={download} href={href}>{text}</Link>
     </div>
   )
 }
