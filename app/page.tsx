3068369a6543fb278c678846824aa3f37a087eac
deletedUser <86692796+DeletedUser9@users.noreply.github.com>
2026-07-11 02:12:06 +1200

    fix: fixed home navigation

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,12 @@ import Reveal from "./components/GeneralComponents/Reveal";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between px-4 sm:px-8 lg:px-24 pt-24">
+      <main
+        id="home"
+        className="flex min-h-screen flex-col items-center justify-between px-4 sm:px-8 lg:px-24 pt-24"
+      >
         <Reveal className="w-full lg:w-full md:h-screen">
-          <section id="home">
+          <section>
             <Hero />
           </section>
         </Reveal>
